6ae6e6c81c7d423f4b0e5038c8f54300284e2cb9
Charitra Agarwal <charitraagarwal1022@gmail.com>
2021-10-24 23:29:39 +0530

    added smooth scrolling

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,3 +1,39 @@
+/* SCROLL jQuery code for smooth animation/scrolling */
+var duration = 300;     //miliseconds latency for animation. More value, slower the animation
+
+$('a').on('click', function(event){     // animate all the anchor tags
+    if(this.hash!==''){
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, duration, function(){
+                window.location.hash = hash;
+        });
+    }/* end-if */
+}); /* on() */
+
+
+/* SCROLL TO TOP jQuery code for smooth animation and fade effects */
+var offset = 200;       //pixels for triggering the button. More the value, more scrolling needed to trigger the button
+
+$(window).scroll(function(){
+    if($(this).scrollTop() > offset){
+        $("#scrolltotop").fadeIn(duration);     // hide the scroll button
+    } else{
+        $("#scrolltotop").fadeOut(duration);    // unhide the scrolling button
+    }
+});
+
+$("#scrolltotop").click(function(event){
+    event.preventDefault();
+    $("html, body").animate({scrollTop: 0}, duration);  // animate the scrolling event
+    return false;
+});
+
+
+
 // Shows the carousel control icons when carousel is hovered
 $(function() {
     $('#carouselCover').hover(
@@ -11,3 +47,4 @@ $(function() {
         }
     );
 });
+
