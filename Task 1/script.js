bf6fc2ba4d87577a410376788e67b03a4a371208
Charitra Agarwal <charitraagarwal1022@gmail.com>
2021-10-25 17:45:31 +0530

    added navbar section update

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,23 +1,9 @@
-/* SCROLL jQuery code for smooth animation/scrolling */
-var duration = 300;     //miliseconds latency for animation. More value, slower the animation
-
-$('a').on('click', function(event){     // animate all the anchor tags
-    if(this.hash!==''){
-        event.preventDefault();
-        var hash = this.hash;
-
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-            }, duration, function(){
-                window.location.hash = hash;
-        });
-    }/* end-if */
-}); /* on() */
-
-
 /* SCROLL TO TOP jQuery code for smooth animation and fade effects */
+var duration = 300;     //miliseconds latency for animation. More value, slower the animation
 var offset = 200;       //pixels for triggering the button. More the value, more scrolling needed to trigger the button
+$('#scrolltotop').fadeOut(duration)
 
+// Hide/show scroll to top button
 $(window).scroll(function(){
     if($(this).scrollTop() > offset){
         $("#scrolltotop").fadeIn(duration);     // hide the scroll button
@@ -26,11 +12,13 @@ $(window).scroll(function(){
     }
 });
 
+// Define click behaviour of scroll to top button
 $("#scrolltotop").click(function(event){
     event.preventDefault();
     $("html, body").animate({scrollTop: 0}, duration);  // animate the scrolling event
     return false;
 });
+
 
 
 
@@ -48,3 +36,25 @@ $(function() {
     );
 });
 
+
+
+
+// Navigation link status update
+var nav_sections = $(".section"); // gets a list of the elements with section class
+var main_nav = $(".navbar");    // get the navigation bar
+
+$(window).on("scroll", function () {
+    var cur_pos = $(this).scrollTop() + 200;
+    nav_sections.each(function () {
+        var top = $(this).offset().top, bottom = top + $(this).outerHeight();
+        if (cur_pos >= top && cur_pos <= bottom) {
+            if (cur_pos <= bottom) {
+                main_nav.find("li").find("a").removeClass("active");
+            }
+            main_nav.find('a[href="#' + $(this).attr("id") + '"]').addClass("active");
+        }
+        if (cur_pos < 300) {
+            $(".navbar ul:first li:first a").addClass("active");
+        }
+    });
+});
